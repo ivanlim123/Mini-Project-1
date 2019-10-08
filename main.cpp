8e6c2ce3d617c945a95c5e422e4598cbd782d3b2
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2019-10-08 10:20:20 +0800

    First Commit
    Create main file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mini Project 1 Tetris
+//
+//  Created by Ivan Lim on 08/10/2019.
+//  Copyright © 2019 Ivan Lim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
