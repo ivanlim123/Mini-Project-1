d8f61349e311b8daf9641f039c2cdd4f01480000
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2019-10-08 10:45:17 +0800

    initiliaze array

--- main.cpp
+++ main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void initArray(int **array, int row, int col);
+
+int main() {
+    int row = 0, col = 0;
+    cin>>row>>col;
+    
+    // 1-index array
+    row += 1;
+    col += 1;
+    
+    
+    int **array = new int*[row];
+    for(int i = 0; i < row; i++) {
+        array[i] = new int[col];
+    }
+    initArray(array, row, col);
+    
+    string block = "";
+    int initCol = 0;
+    while(cin>>block>>initCol) {
+        
+    }
+    
+    for(int i = 1; i < row; i++) {
+        for(int j = 1; j < col; j++) {
+            cout<<array[i][j];
+        }
+        cout<<endl;
+    }
     return 0;
+}
+
+void initArray(int **array, int row, int col) {
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            array[i][j] = 0;
+        }
+    }
 }
